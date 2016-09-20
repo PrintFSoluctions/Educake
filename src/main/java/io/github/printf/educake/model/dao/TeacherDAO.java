e0400f5bf5dcfc65cdb5be0aa80dd85c651e9fcf
Thaila <Thaila@THAILANB>
2016-09-20 19:28:36 -0300

    Classe Teacher adicionada, junto com sua classe DAO correspondente (TeacherDAO)

--- src/main/java/io/github/printf/educake/model/dao/TeacherDAO.java
+++ src/main/java/io/github/printf/educake/model/dao/TeacherDAO.java
@@ -5,10 +5,27 @@
  */
 package io.github.printf.educake.model.dao;
 
+import java.util.List;
+
 /**
  *
  * @author Thaila
  */
-public class TeacherDAO {
+public class TeacherDAO extends DataAccessObject{
+
+    @Override
+    public List findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean removeById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
