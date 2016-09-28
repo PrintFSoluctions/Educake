c3cd0fcfb5b607251ee3cd3d3be904de13bdc23d
AlbinoFreitas <albino_freitas@hotmail.com>
2016-09-28 10:40:13 -0300

    Update code.

--- src/main/java/io/github/printf/educake/util/EnumPhone.java
+++ src/main/java/io/github/printf/educake/util/EnumPhone.java
@@ -5,15 +5,5 @@ package io.github.printf.educake.util;
  * @author Albino Freitas
  */
 public enum EnumPhone {
-    FIXO('F'), CELULAR('C');
-    
-    private char definition;
-    
-    EnumPhone(char definition){
-        this.definition = definition;
-    }
-    
-    public char getDefinition(){
-        return this.definition;
-    }
+    F, C;
 }
