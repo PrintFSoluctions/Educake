19f8dd31b3dd52c9b63d2b3aa4d8a215ab0deee4
Pliavi <pliavi.rpgm@gmail.com>
2016-10-10 11:34:22 -0300

    New form generator test

--- src/main/java/io/github/printf/educake/view/student/NewStudentView.java
+++ src/main/java/io/github/printf/educake/view/student/NewStudentView.java
@@ -1,89 +1,94 @@
 package io.github.printf.educake.view.student;
 
 import de.craften.ui.swingmaterial.MaterialButton;
-import de.craften.ui.swingmaterial.MaterialColor;
 import de.craften.ui.swingmaterial.MaterialComboBox;
 import de.craften.ui.swingmaterial.MaterialTextField;
 import de.craften.ui.swingmaterial.toast.ToastBar;
 import io.github.printf.educake.controller.StudentController;
+import io.github.printf.educake.util.Components.MaterialFormattedTextField;
 import io.github.printf.educake.util.DefaultFormPanel;
-import io.github.printf.educake.util.EasyComponent;
 import jiconfont.icons.GoogleMaterialDesignIcons;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
-import static io.github.printf.educake.util.DefaultFormPanel.Div.BODY;
-import static io.github.printf.educake.util.DefaultFormPanel.Flex.HORIZONTAL;
-import static io.github.printf.educake.util.DefaultFormPanel.Flex.NONE;
+import static io.github.printf.educake.util.Enums.Division.BODY;
+import static io.github.printf.educake.util.Enums.Division.HEADER;
+import static io.github.printf.educake.util.Enums.Flex.BOTH;
+import static io.github.printf.educake.util.Enums.Flex.HORIZONTAL;
+import static io.github.printf.educake.util.Enums.Flex.NONE;
 
 
 /**
  * Created by Vitor on 02/10/2016.
  */
 public class NewStudentView extends JFrame{
-	DefaultFormPanel formPanel = new DefaultFormPanel();
-	MaterialButton addButton = new MaterialButton();
-	JLabel accIcon, birthIcon;
-	public static MaterialTextField
-			nameTextField = new MaterialTextField(),
-			surnameTextField = new MaterialTextField(),
-			birthDateTextField = new MaterialTextField();
-	MaterialComboBox<String> a= new MaterialComboBox();
+	DefaultFormPanel phonesPanel, formPanel;
+	MaterialButton addButton, plusButton;
+	public static MaterialTextField nameTextField, surnameTextField;
+	public static MaterialFormattedTextField birthDateTextField;
+	public static ArrayList<MaterialFormattedTextField> phoneTextField = new ArrayList<>();
+	public static ArrayList<MaterialComboBox> phoneTypeCombo = new ArrayList<>();
 	public static ToastBar errorLog;
 
-	public NewStudentView(){
+	public NewStudentView() throws Exception {
+		formPanel = new DefaultFormPanel();
 		setLayout(new GridLayout()); // Frame Layout
 		add(formPanel); // Add the Panel to the Frame
 
-		// Icons
-		accIcon = EasyComponent.makeIcon(GoogleMaterialDesignIcons.ACCOUNT_BOX);
-		birthIcon = EasyComponent.makeIcon(GoogleMaterialDesignIcons.TODAY);
-
-		nameTextField.setLabel("Nome:");
-		surnameTextField.setLabel("Sobrenome:");
-
-		birthDateTextField.setLabel("Data de Nascimento:");
-		birthDateTextField.setHint("dd/mm/aaaa");
-
-		addButton.setText("Adicionar");
-		addButton.setForeground(MaterialColor.WHITE);
-		addButton.setBackground(MaterialColor.TEAL_400);
-
 		// Components grid
-		try {
-			formPanel.makeGrid(BODY, NONE).addComponent(accIcon);
-			formPanel.makeGrid(BODY, HORIZONTAL).addComponent(nameTextField);
-			formPanel.makeGrid(BODY, HORIZONTAL).addComponent(surnameTextField);
-			formPanel.addRow();
-			formPanel.makeGrid(BODY, NONE).addComponent(birthIcon);
-			formPanel.makeGrid(BODY, HORIZONTAL).setWidth(2).addComponent(birthDateTextField);
-			formPanel.addRow();
+		formPanel.makeGrid(HEADER, HORIZONTAL).addTitleLabel("Cadastro de Aluno");
+		addButton = formPanel.makeGrid(HEADER, HORIZONTAL).addTitleButton("Salvar");
 
-			a.addItem("teste1");
-			a.addItem("teste2");
-			a.addItem("teste3");
-			formPanel.makeGrid(BODY, HORIZONTAL).setWidth(3).setHeight(10).addComponent(a);
-
-			formPanel.addRow();
-			formPanel.makeGrid(BODY, HORIZONTAL).setWidth(3).addComponent(addButton);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		formPanel.makeGrid(BODY, NONE).addIcon(GoogleMaterialDesignIcons.ACCOUNT_BOX);
+		nameTextField = formPanel.makeGrid(BODY, HORIZONTAL).addTextField("Nome:");
+		surnameTextField = formPanel.makeGrid(BODY, HORIZONTAL).addTextField("Sobrenome:");
+		formPanel.addRow();
+		formPanel.makeGrid(BODY, NONE).addIcon(GoogleMaterialDesignIcons.TODAY);
+		birthDateTextField = formPanel.makeGrid(BODY, HORIZONTAL).setWidth(2)
+				.addFormattedTextField("Data de Nascimento", "00/00/0000", "##/##/####");
+		formPanel.addRow();
+		phonesPanel = formPanel.makeGrid(BODY, BOTH).setWidth(3).addInnerPanel();
+		phoneTextField.add(phonesPanel.makeGrid(BODY,HORIZONTAL).setGridWidth(6)
+				.addFormattedTextField("Telefone:","(00)000000000","(##)#########"));
+		phoneTypeCombo.add(phonesPanel.makeGrid(BODY,HORIZONTAL)
+				.addComboBox("Fixo", "Celular"));
+		formPanel.addRow();
+		plusButton = formPanel.makeGrid(BODY, HORIZONTAL).setWidth(3).addButton("Adicionar Novo Telefone");
 
 		init(); // iniatlize all
 	}
 
 	private void init(){
-//		initButtons();
-		setMinimumSize(new Dimension(600,100));
+		initButtons();
+		setMinimumSize(new Dimension(600,400));
+		setMaximumSize(new Dimension(600,400));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		pack();
 		setVisible(true);
 	}
+
 	private void initButtons(){
 		addButton.addActionListener(StudentController.getAddListener());
+		plusButton.addActionListener(e -> {
+			System.out.println("b");
+			try {
+				System.out.println("a");
+				phonesPanel.addRow();
+				phoneTextField.add(phonesPanel.makeGrid(BODY,HORIZONTAL).setGridWidth(6)
+						.addFormattedTextField("Telefone:","(00)000000000","(##)#########"));
+				phoneTypeCombo.add(phonesPanel.makeGrid(BODY,HORIZONTAL)
+						.addComboBox("Fixo", "Celular"));
+				phonesPanel.updateUI();
+				phonesPanel.getBody().updateUI();
+				formPanel.updateUI();
+
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
 	}
 }
 
