b18ebc71205659ab5f4503ce3cafd75c4fb792b8
Pliavi <pliavi.rpgm@gmail.com>
2016-10-10 11:34:03 -0300

    Added some new features to the generator. like panel getters/setters(adds), inner panels, and dissolved addComponent in a method to each component

--- src/main/java/io/github/printf/educake/util/DefaultFormPanel.java
+++ src/main/java/io/github/printf/educake/util/DefaultFormPanel.java
@@ -1,7 +1,11 @@
 package io.github.printf.educake.util;
 
-import de.craften.ui.swingmaterial.MaterialColor;
-import de.craften.ui.swingmaterial.MaterialPanel;
+import de.craften.ui.swingmaterial.*;
+import io.github.printf.educake.util.Components.MaterialFormattedTextField;
+import io.github.printf.educake.util.Enums.Division;
+import io.github.printf.educake.util.Enums.Flex;
+import jiconfont.icons.GoogleMaterialDesignIcons;
+import jiconfont.swing.IconFontSwing;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,13 +14,12 @@ import java.awt.*;
  * @author Vitor
  * on 06/10/2016.
  */
-public class DefaultFormPanel extends MaterialPanel {
-	private JPanel header      = new JPanel();
-	private JPanel body        = new JPanel(); // Container principal
-	private JScrollPane scroll = new JScrollPane(body); // Container com scrollbar
-	private JPanel footer      = new JPanel();
+public class DefaultFormPanel extends JPanel {
+	private JPanel footer, body;
+	private JPanel header;
+	public JScrollPane scroll = new JScrollPane(body); // Container com scrollbar
 	private JPanel selectedPanel;
-	private GridBagConstraints constraint;
+	public GridBagConstraints constraint;
 	private boolean isGridMade = false;
 	private int y = 0, x = 0;
 
@@ -25,47 +28,38 @@ public class DefaultFormPanel extends MaterialPanel {
 	}
 
 	private void initPanel(){
+		IconFontSwing.register(GoogleMaterialDesignIcons.getIconFont()); // Registra a fonte de icones
 		this.setLayout(new BorderLayout());
 		this.setBackground(MaterialColor.WHITE);
-
-		header.setLayout(new GridBagLayout());
-		header.setBackground(MaterialColor.RED__50);
-
-		body.setLayout(new GridBagLayout());
-		body.setBackground(MaterialColor.WHITE);
-
-		scroll.getViewport().setBackground(MaterialColor.WHITE);
-		scroll.setBorder(null);
-
-		footer.setLayout(new GridBagLayout());
-		footer.setBackground(MaterialColor.BLUE_50);
-
-		this.add(header, BorderLayout.NORTH);
-		this.add(scroll, BorderLayout.CENTER);
-		this.add(footer, BorderLayout.SOUTH);
-
 	}
-
-	/** Adds one to the grid Y and resets X, to make a new row */
-	public void addRow(){y++; x = 0;}
 
 	/**
 	 * Create a new space to a possibly component that will come into it.
-	 * @param where What division the possibly component will go
+	 * @param division What division the possibly component will go
 	 * @param flex What kind of full resize it will get, or None
 	 *
 	 * @return DefaultFormPanel The own panel, to give the possibility of cascade methods
 	 * */
-	public DefaultFormPanel makeGrid(Div where, Flex flex){
+	public DefaultFormPanel makeGrid(Division division, Flex flex) throws Exception {
 		constraint = new GridBagConstraints();
+		selectedPanel = selectPanelByDivision(division);
 
-		switch (where){
-			case HEADER: selectedPanel = header; break;
-			case BODY:   selectedPanel = body; break;
-			case FOOTER: selectedPanel = footer; break;
+		if (selectedPanel == null) {
+			x = y = 0;
+			if (division == Division.HEADER) {
+				addHeader();
+				selectedPanel = header;
+			} else if(division == Division.BODY) {
+				addBody();
+				selectedPanel = body;
+			}else{
+				addFooter();
+				selectedPanel = footer;
+			}
 		}
 
 		constraint.ipady = 54; // Altura padrão dos components
+
 		constraint.insets = new Insets(0,5,0,0); // Borda Padrão
 		constraint.fill = flex.getFlex();
 		constraint.gridx = x++; // coloca na posição e já adiciona +1 para o próximo componente da linha
@@ -86,6 +80,158 @@ public class DefaultFormPanel extends MaterialPanel {
 		return this;
 	}
 
+
+	/** Adds one to the grid Y and resets X, to make a new row */
+	public void addRow(){
+		y++; x = 0;
+	}
+
+	private void addHeader() throws Exception {
+		if(header == null){
+			header = new JPanel();
+
+			header.setLayout(new GridBagLayout());
+			header.setBackground(MaterialColor.TEAL_500);
+
+			this.add(header, BorderLayout.NORTH);
+		}else{
+			throw new Exception("Já existe um CABEÇALHO!");
+		}
+	}
+
+	private void addBody() throws Exception {
+		if(body == null){
+			body = new JPanel();
+
+			scroll = new JScrollPane(body);
+
+			body.setLayout(new GridBagLayout());
+			body.setBackground(MaterialColor.WHITE);
+			body.setBorder(BorderFactory.createEmptyBorder(0,10,0,10));
+
+			scroll.setBorder(null);
+
+			this.add(scroll);
+		}else{
+			throw new Exception("Já existe um CORPO!");
+		}
+	}
+
+	private void addBodyWithoutScroll() throws Exception{
+		if(body == null){
+			body = new JPanel();
+
+			body.setLayout(new GridBagLayout());
+			body.setBackground(MaterialColor.WHITE);
+			body.setBorder(BorderFactory.createEmptyBorder(0,10,0,10));
+
+			this.add(body);
+		}else{
+			throw new Exception("Já existe um CORPO!");
+		}
+	}
+
+	private void addFooter() throws Exception {
+		if(footer == null){
+			footer = new JPanel();
+
+			footer.setLayout(new GridBagLayout());
+			footer.setBackground(MaterialColor.WHITE);
+
+			this.add(footer, BorderLayout.SOUTH);
+		}else{
+			throw new Exception("Já existe um RODAPÉ!");
+		}
+	}
+
+	public DefaultFormPanel addIcon(GoogleMaterialDesignIcons iconName) throws Exception {
+		Icon iconFont = IconFontSwing.buildIcon(iconName, 32, MaterialColor.TEAL_400);
+
+		JLabel icon = new JLabel();
+		icon.setIcon(iconFont);
+
+		addComponent(icon);
+		return this;
+	}
+
+	public void addLabel(String text) throws Exception{
+		JLabel label = new JLabel(text);
+		label.setFont(Roboto.REGULAR.deriveFont(14.0F));
+		this.addComponent(label);
+	}
+
+	public void addTitleLabel(String text) throws Exception{
+		JLabel label = new JLabel("     "+text);
+		label.setFont(Roboto.BOLD.deriveFont(26.0F));
+		label.setForeground(MaterialColor.WHITE);
+		this.addComponent(label);
+	}
+
+
+	public MaterialTextField addTextField(String label) throws Exception{
+		MaterialTextField textField = new MaterialTextField();
+		textField.setLabel(label);
+		this.addComponent(textField);
+		return textField;
+	}
+
+	public MaterialTextField addTextField(String label, String hint) throws Exception{
+		MaterialTextField textField = addTextField(label);
+		textField.setHint(hint);
+		return textField;
+	}
+
+	public MaterialFormattedTextField addFormattedTextField(String label, String mask) throws Exception {
+		MaterialFormattedTextField textField = new MaterialFormattedTextField(mask);
+		textField.setLabel(label);
+		addComponent(textField);
+		return textField;
+	}
+
+	public MaterialFormattedTextField addFormattedTextField(String label, String hint, String mask) throws Exception {
+		MaterialFormattedTextField textField = addFormattedTextField(label, mask);
+		textField.setHint(hint);
+		return textField;
+	}
+
+	public MaterialComboBox addComboBox(String... items) throws Exception {
+		MaterialComboBox combo = new MaterialComboBox();
+		constraint.ipady = 39; // o Combo tem tamanho diferente
+		for (String item : items) combo.addItem(item);
+		addComponent(combo);
+		return combo;
+	}
+
+	public MaterialButton addTitleButton(String label) throws Exception {
+		MaterialButton button = new MaterialButton();
+		button.setText(label);
+		button.setForeground(MaterialColor.WHITE);
+		button.setBackground(MaterialColor.TEAL_300);
+		addComponent(button);
+		return button;
+	}
+
+	public MaterialButton addButton(String label) throws Exception {
+		MaterialButton button = new MaterialButton();
+		button.setText(label);
+		button.setForeground(MaterialColor.WHITE);
+		button.setBackground(MaterialColor.TEAL_500);
+		addComponent(button);
+		return button;
+	}
+
+	private void addComponent(JComponent component) throws Exception {
+		if (!isGridMade) throw new Exception("Crie a grid antes!");
+		selectedPanel.add(component, constraint);
+
+		System.out.println("Added Component: "
+				+ component.getClass().getSimpleName()
+				+ "\n - posX...: " + x
+				+ "\n - posY...: " + y);
+
+		isGridMade = false;
+	}
+
 	public DefaultFormPanel setWidth(int width) throws Exception {
 		if (!isGridMade) throw new Exception("Crie a grid antes!");
 		constraint.gridwidth = width;
@@ -94,35 +240,56 @@ public class DefaultFormPanel extends MaterialPanel {
 	}
 
 	public DefaultFormPanel setHeight(int width) throws Exception {
-		if (!isGridMade) throw new Exception("Crie a grid antes!");
+//		if (!isGridMade) throw new Exception("Crie a grid antes!");
 
 		constraint.ipady = width;
+		updateUI();
+		revalidate();
+		repaint();
 		return this;
 	}
 
-
-	public void addComponent(JComponent component) throws Exception {
+	public DefaultFormPanel setGridWidth(int gridWidth) throws Exception {
 		if (!isGridMade) throw new Exception("Crie a grid antes!");
-		selectedPanel.add(component, constraint);
-		isGridMade = false;
+
+		constraint.ipadx = (getSize().width/100)*gridWidth;
+		constraint.weightx = gridWidth;
+		return this;
 	}
 
-	public enum Div{ HEADER, BODY, FOOTER }
-
-	public enum Flex{
-		HORIZONTAL(GridBagConstraints.HORIZONTAL),
-		VERTICAL(GridBagConstraints.VERTICAL),
-		BOTH(GridBagConstraints.BOTH),
-		NONE(GridBagConstraints.NONE);
-
-		private final int flex;
-
-		Flex(int flex) {
-			this.flex = flex;
+	private JPanel selectPanelByDivision(Division division){
+		switch (division){
+			case HEADER:
+				return header;
+			case BODY:
+				return body;
+			case FOOTER:
+				return footer;
 		}
+		return null;
+	}
 
-		public int getFlex(){
-			return flex;
-		}
+	public JPanel getHeader() {
+		return header;
+	}
+
+	public JPanel getBody() {
+		return body;
+	}
+
+	public JPanel getFooter() {
+		return footer;
+	}
+
+	public void toggleVisibility(Division division) {
+		selectedPanel = selectPanelByDivision(division);
+		selectedPanel.setVisible(!selectedPanel.isVisible());
+	}
+
+	public DefaultFormPanel addInnerPanel() throws Exception {
+		DefaultFormPanel panel = new DefaultFormPanel();
+		panel.addBodyWithoutScroll();
+		addComponent(panel);
+		return panel;
 	}
 }
