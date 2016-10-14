1c2339baa0d110e2ca869eb8df5289a7da65c15d
AlbinoFreitas <albino_freitas@hotmail.com>
2016-10-14 12:32:52 -0300

    Adicionado classe para teste

---
+++ src/main/java/io/github/printf/educake/Teste.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.printf.educake;
+
+import io.github.printf.educake.controller.service.PersonService;
+import io.github.printf.educake.controller.service.StudentService;
+import io.github.printf.educake.model.Person;
+import io.github.printf.educake.model.Student;
+
+/**
+ *
+ * @author albinof
+ */
+public class Teste {
+    public static void main(String... args) {
+        PersonService person = new PersonService();
+        StudentService student = new StudentService();
+        
+        try{
+            person.validateNameAndSurname("Albino", "Freitas");
+            person.validateDate("03/11/1997");
+            
+            student.setPerson(person.getPeson());
+            student.setResponsible(person.getPeson());
+            student.generateRm();
+            student.persist();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }
+}
