df06a42fc36a138c126d48869e39d263036a6f07
Pliavi <pliavi.rpgm@gmail.com>
2016-10-21 09:29:23 -0200

    Created AddressService Base

---
+++ src/main/java/io/github/printf/educake/controller/service/AddressService.java
@@ -0,0 +1,22 @@
+package io.github.printf.educake.controller.service;
+
+import io.github.printf.educake.model.Address;
+import io.github.printf.educake.model.Person;
+
+/**
+ * @author Vitor Silvério de Souza
+ *         On out, 2016
+ */
+
+// TODO: A classe completa
+public class AddressService {
+  private Address address;
+
+  public void persist(Person person, String cep, String street, String city, String state, String housenumber, String complement) {
+    throw  new UnsupportedOperationException("Ainda não implementado");
+  }
+
+  public Address getAddress() {
+    throw  new UnsupportedOperationException("Ainda não implementado");
+  }
+}
