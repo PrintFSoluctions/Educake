9dd5e6c633b3d3106b14f6e5385740f581cd8555
Albino Freitas <albino_freitas@hotmail.com>
2016-10-23 15:29:43 -0200

    Criação da classe TeacherService
    É necessário mudar a model para aceitar chave composta

---
+++ src/main/java/io/github/printf/educake/controller/service/TeacherService.java
@@ -0,0 +1,33 @@
+package io.github.printf.educake.controller.service;
+
+import io.github.printf.educake.model.Person;
+import io.github.printf.educake.model.Teacher;
+import io.github.printf.educake.model.dao.TeacherDAO;
+
+/**
+ *
+ * @author Albino Freitas
+ */
+public class TeacherService {
+    
+    private final Teacher teacher;
+    private final TeacherDAO teacherDAO;
+    
+    public TeacherService() {
+        teacher = new Teacher();
+        teacherDAO = new TeacherDAO();
+    }
+    
+    public void setTeacher(Person person){
+        this.teacher.setPerson(person);
+    }
+    
+    public Teacher getTeacher(){
+        return this.teacher;
+    }
+    
+    public boolean persist(){
+        return teacherDAO.persist(this.teacher);    
+    }
+    
+}
