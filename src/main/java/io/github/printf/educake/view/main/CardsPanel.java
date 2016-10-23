11a45078e4f1ef6958927ff64d8216afdca8b554
Pliavi <pliavi.rpgm@gmail.com>
2016-10-23 01:31:44 -0200

    Added a CardLayout to load all the panels to the main frame

---
+++ src/main/java/io/github/printf/educake/view/main/CardsPanel.java
@@ -0,0 +1,25 @@
+package io.github.printf.educake.view.main;
+
+import io.github.printf.educake.view.person.student.StudentView;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Vitor Silvério de Souza
+ *         On out, 2016
+ */
+public class CardsPanel extends JPanel {
+
+  public CardsPanel() {
+    setLayout(new CardLayout());
+
+    add(new StudentView(), "Student");
+    add(new JButton("Essa é a tela dos Professores"), "Teacher");
+    add(new JButton("Essa é a tela dos Relatórios"), "Reports");
+
+
+
+
+  }
+}
