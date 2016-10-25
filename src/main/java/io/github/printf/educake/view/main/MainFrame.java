574e8f093af121954186ad4a87c3df85c1376a8c
Pliavi <pliavi.rpgm@gmail.com>
2016-10-25 11:13:30 -0200

    Testing menubar

--- src/main/java/io/github/printf/educake/view/main/MainFrame.java
+++ src/main/java/io/github/printf/educake/view/main/MainFrame.java
@@ -13,15 +13,22 @@ public class MainFrame extends JFrame{
   public MainFrame() throws HeadlessException {
     setTitle("Educake");
 
+    JMenuBar bar = new JMenuBar();
+    bar.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+    JMenu menu = new JMenu("{ @UserName }");
+    JMenuItem sair = new JMenuItem("Sair");
+    sair.addActionListener(e -> {this.dispose(); System.exit(0);});
+
+    bar.add(menu);
+    menu.add(sair);
+
     cards = new CardsPanel();
     cards.setMinimumSize(new Dimension(600,400));
     cards.setPreferredSize(new Dimension(600,700));
 
-    JButton c = new JButton("Vertical Menu");
-
     LateralMenu lateralMenu = new LateralMenu(cards);
 
-    add(c, BorderLayout.PAGE_START);
+    add(bar, BorderLayout.PAGE_START);
     add(cards, BorderLayout.CENTER);
     add(lateralMenu, BorderLayout.LINE_START);
 
