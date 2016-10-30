b0b4cd898a9f0ecca959287b047405762b1dae22
Pliavi <pliavi.rpgm@gmail.com>
2016-10-30 00:51:35 -0200

    Changing to new DefaultFormPanel

--- src/main/java/io/github/printf/educake/view/ThumbPanel.java
+++ src/main/java/io/github/printf/educake/view/ThumbPanel.java
@@ -2,6 +2,7 @@ package io.github.printf.educake.view;
 
 import de.craften.ui.swingmaterial.MaterialColor;
 import io.github.printf.educake.util.Components.NewDefaultFormPanel;
+import jiconfont.icons.GoogleMaterialDesignIcons;
 
 import javax.swing.*;
 
@@ -12,10 +13,14 @@ import javax.swing.*;
 public class ThumbPanel extends NewDefaultFormPanel{
 
   public ThumbPanel(String...fields) {
-    setBorder(BorderFactory.createLineBorder(MaterialColor.TEAL_200, 2, true));
+    setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, MaterialColor.TEAL_400));
     for(String field : fields){
       addLabel(field);
     }
+    addIcon(GoogleMaterialDesignIcons.EDIT);
+    cell.width(50).expand(false,false);
+    addIcon(GoogleMaterialDesignIcons.DELETE);
+    cell.width(50).expand(false,false);
   }
 
 }
