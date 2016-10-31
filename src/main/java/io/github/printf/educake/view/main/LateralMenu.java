90c9adb5251be984f81b378b7a7b848c7eca0fc1
Pliavi <pliavi.rpgm@gmail.com>
2016-10-31 00:05:13 -0200

    Now using the ComponentFactory and the MigLayout

--- src/main/java/io/github/printf/educake/view/main/LateralMenu.java
+++ src/main/java/io/github/printf/educake/view/main/LateralMenu.java
@@ -2,9 +2,8 @@ package io.github.printf.educake.view.main;
 
 import de.craften.ui.swingmaterial.MaterialButton;
 import de.craften.ui.swingmaterial.MaterialColor;
-import io.github.printf.educake.util.Components.DefaultFormPanel;
-import io.github.printf.educake.util.Enums.Division;
-import io.github.printf.educake.util.Enums.Flex;
+import io.github.printf.educake.util.Components.ComponentFactory;
+import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,7 +14,7 @@ import java.util.List;
  * @author Vitor Silvério de Souza
  *         On out, 2016
  */
-public class LateralMenu extends DefaultFormPanel {
+public class LateralMenu extends JPanel {
   private final String[][] panelNames = new String[][]{
       {"Student", "Aluno"},
       {"Teacher", "Professor"},
@@ -23,38 +22,31 @@ public class LateralMenu extends DefaultFormPanel {
   };
 
   CardLayout cardLayout;
-  private List<MaterialButton> buttons = new ArrayList<MaterialButton>();
+  private List<MaterialButton> buttons = new ArrayList<>();
+  private final ComponentFactory component = new ComponentFactory();
 
 
   public LateralMenu(CardsPanel cards) {
     cardLayout = (CardLayout) cards.getLayout();
+    JLabel educakeLogo = component.addIcon("logo.png");
 
-    try {
-      ImageIcon icon = new ImageIcon("src/main/resources/Images/logo.png");
+    setLayout(new MigLayout("flowy, novisualpadding","",""));
+    setBackground(MaterialColor.TEAL_600);
+    setPreferredSize(new Dimension(150, 200));
 
-      JLabel educakeLogo = makeGrid(Division.HEADER, Flex.HORIZONTAL)
-          .setHeight(16).addLabel("");
-      educakeLogo.setHorizontalAlignment(JLabel.CENTER);
-      educakeLogo.setVerticalAlignment(JLabel.CENTER);
-
-      educakeLogo.setIcon(icon);
-      addRow();
-
-      setBackground(MaterialColor.TEAL_600);
-      getHeader().setBackground(MaterialColor.TEAL_600);
+    add(educakeLogo, "h 70px!, x visual.width/2-20px");
 
       for (int i = 0; i < panelNames.length; i++) {
         String[] panelName = panelNames[i];
-        buttons.add(makeGrid(Division.HEADER, Flex.HORIZONTAL)
-            .setInset(-10,-0,-10,-20)
-            .addTitleButton(panelName[1]));
-        buttons.get(i).addActionListener(e -> cardLayout.show(cards, panelName[0]));
-        addRow();
+
+        MaterialButton button = component.addTitleButton(panelName[1]);
+
+        buttons.add(button);
+        String constraints = "h 70px!, w 180px!, x -10px, y 76+"+(i*50);
+        add(button, constraints);
+
+        button.addActionListener(e -> cardLayout.show(cards, panelName[0]));
       }
 
-      setPreferredSize(new Dimension(150, 200));
-    }catch (Exception ex){
-      ex.printStackTrace();
-    }
   }
 }
--- src/main/java/io/github/printf/educake/view/person/student/StudentDash.java
+++ src/main/java/io/github/printf/educake/view/person/student/StudentDash.java
@@ -1,28 +1,33 @@
 package io.github.printf.educake.view.person.student;
 
-import io.github.printf.educake.util.Components.NewDefaultFormPanel;
-import io.github.printf.educake.view.ThumbPanel;
+import de.craften.ui.swingmaterial.MaterialColor;
+import io.github.printf.educake.util.Components.ComponentFactory;
+import io.github.printf.educake.util.Components.ThumbPanel;
+import net.miginfocom.swing.MigLayout;
 
-import java.util.ArrayList;
+import javax.swing.*;
 
 /**
  * @author Vitor Silvério de Souza
  *         On out, 2016
  */
-public class StudentDash extends NewDefaultFormPanel {
+public class StudentDash extends JPanel{
+  private final ComponentFactory component = new ComponentFactory();
 
-  ArrayList<ThumbPanel>  students = new ArrayList<ThumbPanel>();
+//  ArrayList<ThumbPanel>  students = new ArrayList<ThumbPanel>();
 
   public StudentDash() {
+    setLayout(new MigLayout("insets 4 4 4 4","", ""));
+    setBackground(MaterialColor.GREEN_200);
 
-    top();
-    addCell(new ThumbPanel("a321556", "Albino Freitas", "29/09/1995", "Caraguatatuba"));
-    row();
-    addCell(new ThumbPanel("a321556", "Jhones Henrique", "29/09/1995", "Caraguatatuba"));
-    row();
-    addCell(new ThumbPanel("a321556", "Thaila Golçalves", "29/09/1995", "Caraguatatuba"));
-    row();
-    addCell(new ThumbPanel("a32155x", "Vitor Silvério", "29/09/1995", "Caraguatatuba"));
+    add(component.header("Alunos", "Novo Aluno"), "north");
+
+    String constraints = "w 100%, wrap";
+    add(new ThumbPanel("a321654x", "Albino Freitas", "29/09/1995"), constraints);
+    add(new ThumbPanel("a321654x", "Jhones Henrique", "29/09/1995"), constraints);
+    add(new ThumbPanel("a321654x", "Vitor Silvério", "29/09/1995"), constraints);
+    add(new ThumbPanel("a321654x", "Thaila Gonçalves", "29/09/1995"), constraints);
+
   }
 
 }
