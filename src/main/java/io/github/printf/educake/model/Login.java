54fcd7147701ab2897e3e3b4cd5e708d0edd33d0
a1402056 <a1402056@LAB112M20>
2016-11-11 21:15:58 -0200

    Login Class Mapping

---
+++ src/main/java/io/github/printf/educake/model/Login.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.printf.educake.model;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author a1402056
+ */
+public class Login {
+    @Id
+    @Column
+    private String user;
+    
+    @Column
+    private String password;
+
+    /**
+     * @return the user
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+ 
+}
