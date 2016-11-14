a2bfa3996e666c758e28fcfcfcd58f9b434a9cae
Pliavi <pliavi.rpgm@gmail.com>
2016-11-14 17:35:46 -0200

    added login method using BCrypt hash

--- src/main/java/io/github/printf/educake/model/dao/LoginDAO.java
+++ src/main/java/io/github/printf/educake/model/dao/LoginDAO.java
@@ -6,6 +6,9 @@
 package io.github.printf.educake.model.dao;
 
 import io.github.printf.educake.model.Login;
+import io.github.printf.educake.util.BCrypt;
+import org.hibernate.Query;
+
 import java.util.List;
 
 /**
@@ -13,6 +16,7 @@ import java.util.List;
  * @author a1402056
  */
 public class LoginDAO extends DataAccessObject<Login> {
+    Login login = new Login();
 
     @Override
     public List<Login> findAll() {
@@ -22,17 +26,36 @@ public class LoginDAO extends DataAccessObject<Login> {
     public Login getByUser(String user) {
         return getSession().load(Login.class, user);
     }
- 
+
     public boolean removeByUser(String user) {
-         boolean result = true;
-      try {
-          Login login = this.getByUser(user);
-          super.remove(login);
-      } catch (Exception e) {
-          e.printStackTrace();
-          result = false;
-      }    
-    return result;
+        boolean result = true;
+        try {
+            login = this.getByUser(user);
+            super.remove(login);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = false;
+        }
+        return result;
+    }
+
+    public boolean tryLogin(String user, String password){
+        boolean result = true;
+
+        Query query = getSession().createQuery("FROM Login WHERE user = :user");
+        query.setParameter("user", user);
+
+        login = (Login) query.uniqueResult();
+
+        if(login != null){
+            result = BCrypt.checkpw(password, login.getPassword());
+            System.out.println("senha "+((!result)?"in":"")+"correta");
+        }else{
+            System.out.println("não existe usuário");
+            result = false;
+        }
+
+        return result;
     }
 
     @Override
@@ -44,5 +67,5 @@ public class LoginDAO extends DataAccessObject<Login> {
     public boolean removeById(Integer id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
