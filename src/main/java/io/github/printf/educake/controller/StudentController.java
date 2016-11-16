bce561accd19fdc144185169873b72c8dd278eaf
Pliavi <pliavi.rpgm@gmail.com>
2016-11-15 23:54:56 -0200

    added getByID Method

--- src/main/java/io/github/printf/educake/controller/StudentController.java
+++ src/main/java/io/github/printf/educake/controller/StudentController.java
@@ -7,6 +7,7 @@ import io.github.printf.educake.controller.service.StudentService;
 import io.github.printf.educake.model.Address;
 import io.github.printf.educake.model.Person;
 import io.github.printf.educake.model.Phone;
+import io.github.printf.educake.model.Student;
 import io.github.printf.educake.view.main.MainFrame;
 import io.github.printf.educake.view.person.AddressPanel;
 import io.github.printf.educake.view.person.PersonPanel;
@@ -17,6 +18,8 @@ import io.github.printf.educake.view.person.student.StudentView;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -73,7 +76,7 @@ public class StudentController {
         if(studentService.isOfAge() || studentService.getResponsible() != null){
           studentService.persist();
         }else{
-          MainFrame.goTo("attachResponsible");
+          MainFrame.goTo("attachResponsible", person.getIdPerson());
         }
 
       }catch (Exception ex){
@@ -81,6 +84,67 @@ public class StudentController {
         studentService.rollback();
       }
     };
+  }
+
+  public ActionListener update() {
+    return (ActionEvent e) -> {
+      String name = personPanel.getName();
+      String surname = personPanel.getSurname();
+      String birthDate = personPanel.getBirth();
+
+      List<String> types = phonePanel.getPhoneTypes();
+      List<String> tels = phonePanel.getPhones();
+
+      String CEP = addressPanel.getCEP();
+      String street = addressPanel.getStreet();
+      String city = addressPanel.getCity();
+      String state = addressPanel.getState();
+      String housenumber = addressPanel.getHouseNumber();
+      String complement = addressPanel.getComplement();
+
+      try {
+        phonesService.setPhones(tels, types);
+        addressService.setAddress(street, housenumber, complement, city, CEP, state);
+        personService.setPerson(name, surname, birthDate);
+        System.out.println(name + " " + surname + " : " + birthDate);
+
+        phones = phonesService.getPhones();
+        address = addressService.getAddress();
+        person = personService.getPerson();
+
+        person.setPhones(phones);
+        person.setAddress(address);
+
+        Student temporaryStudent = studentService.getStudent();
+        Student editedStudent = studentService.getStudentById(studentView.getId());
+        temporaryStudent.setRm(editedStudent.getRm());
+
+        studentService.setStudent(temporaryStudent);
+        studentService.update();
+      }catch (Exception ex){
+        JOptionPane.showMessageDialog(null, ex.getMessage());
+        studentService.rollback();
+      }
+    };
+  }
+
+  public MouseAdapter delete(int id){
+    return new MouseAdapter() {
+      @Override
+      public void mousePressed(MouseEvent e) {
+        int opt = JOptionPane.showConfirmDialog(null,
+            "Tem certeza que deseja remover este aluno?", "Remoção de Aluno",
+            JOptionPane.YES_NO_OPTION);
+
+        if (opt == JOptionPane.YES_OPTION){
+          studentService.remove(id);
+        }
+      }
+    };
+  }
+
+  public Student getStudentById(int id){
+    return studentService.getStudentById(id);
   }
 
   public void setView(StudentView view) {
