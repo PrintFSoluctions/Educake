68d3f28e7a8b9f7ebb65eb15855476f22f64b8cb
Pliavi <pliavi.rpgm@gmail.com>
2016-11-16 00:00:23 -0200

    Added a method to convert a date into a ddMMyyyy date (without traces or bars)

--- src/main/java/io/github/printf/educake/util/EasyDate.java
+++ src/main/java/io/github/printf/educake/util/EasyDate.java
@@ -1,5 +1,6 @@
 package io.github.printf.educake.util;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -24,5 +25,18 @@ public class EasyDate {
 		Date rearrangedDate = toDate(day,month,year);
 
 		return rearrangedDate;
+	}
+
+	public static String toString(Date date){
+		String result;
+		GregorianCalendar gc = new GregorianCalendar();
+
+		date.getTime();
+		gc.setTime(date);
+		result = String.valueOf(gc.get(Calendar.DAY_OF_MONTH));
+		result += gc.get(Calendar.MONTH);
+		result += gc.get(Calendar.YEAR);
+
+		return result;
 	}
 }
