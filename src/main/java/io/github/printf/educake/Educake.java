248e26afba47e9059e6e34f42d16eb740ae2a544
a150276x <a150276x@LAB112M19>
2016-11-21 20:32:42 -0200

    Testing JFXML

--- src/main/java/io/github/printf/educake/Educake.java
+++ src/main/java/io/github/printf/educake/Educake.java
@@ -1,9 +1,51 @@
 package io.github.printf.educake;
 
-public class Educake {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public static void main(String... args) {
-        System.out.println("Diga-me o que fazer!");
+public class Educake extends Application {
+
+    public static String loginID = "login";
+    public static String loginFile = "login.fxml";
+
+//    public static String mainID = "main";
+//    public static String mainFile = "main.fxml";
+//
+//    public static String addStudentID = "addStudent";
+//    public static String addStudentFile = "addStudent.fxml";
+//
+//    public static String addPaymentID = "addPayment";
+//    public static String addPaymentFile = "addPayment.fxml";
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        ScreensController mainContainer = new ScreensController();
+        mainContainer.loadScreen(loginID, loginFile);
+//        mainContainer.loadScreen(this.mainID, this.mainFile);
+//        mainContainer.loadScreen(this.addStudentID, this.addStudentFile);
+//        mainContainer.loadScreen(this.addPaymentID, this.addStudentFile);
+
+        mainContainer.setScreen(loginID);
+
+        Group root = new Group();
+        root.getChildren().addAll(mainContainer);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/io/github/printf/educake/controller/LoginController.java
+++ src/main/java/io/github/printf/educake/controller/LoginController.java
@@ -1,17 +1,44 @@
 package io.github.printf.educake.controller;
 
+import io.github.printf.educake.ScreensController;
 import io.github.printf.educake.dao.LoginDAO;
+import io.github.printf.educake.view.ControlledScreen;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
- * @author Vitor Silvério de Souza
- *         On nov, 2016
+ * @author Vitor Silvério de Souza On nov, 2016
  */
-public class LoginController {
+public class LoginController implements Initializable, ControlledScreen {
 
+    @FXML
+    private TextField user;
+    @FXML
+    private PasswordField pass;
+    ScreensController myController;
     LoginDAO dao = new LoginDAO();
 
-    public boolean tryLogin(String user, String pass){
-        return dao.tryLogin(user, pass);
+    @Override
+    public void setScreenParent(ScreensController screenParent) {
+        myController = screenParent;
+    }
+
+    @FXML
+    public boolean tryLogin() {
+        String username = user.getText();
+        String password = pass.getText();
+
+        return dao.tryLogin(username, password);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
